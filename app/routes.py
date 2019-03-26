3cb7c2c063411bc779a8ae17a8f47a52154e9942
JustinJohnsonK <justinjohnsonkomban@gmail.com>
2019-03-26 08:32:42 +0530

    second-major-commit-fixed-csv-file-disorder

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,7 @@ filepath = os.path.join(os.path.dirname(__file__), 'packets.csv')
 
 def readFile():
     # open_read = open(filepath, 'r')
-    df = pd.read_csv(filepath, delimiter='","|,|"')
+    df = pd.read_csv(filepath, delimiter='\\')
     # df = pd.DataFrame.from_csv(filepath)
     return df
     # page = ''
@@ -28,6 +28,7 @@ def index():
     while(True):
         page = readFile()
         return render_template("index.html", title="DDOS Analyser", packets=page.to_html(), attack=detect_attack)
+        index()
 
 @app.route('/packets')
 def incomming_packets():
