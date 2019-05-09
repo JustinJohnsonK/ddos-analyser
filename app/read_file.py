2d4308441d8f52c35c0efdca988d8191a4fc923a
JustinJohnsonK <justinjohnsonkomban@gmail.com>
2019-05-09 22:17:53 +0530

    packet sniffing using pyshark

--- app/read_file.py
+++ app/read_file.py
@@ -1,16 +1,114 @@
-# reads the input file and transforms it. 
 import pandas as pd
+import pyshark
+import csv
+import time
+from timeit import default_timer as timer
 
-class read_file:
+def main():
+    allowed_IP = ['192.168.1.1', '192.168.1.2', '192.168.1.3', '192.168.1.4']
+    fileName = 'app/LiveAnn.csv'
+    capture = pyshark.LiveCapture(interface='any')
+    capture.sniff_continuously(packet_count=10)
+    # for packet in capture.sniff_continuously(packet_count=None):
+    #     print('Just arrived:', packet)
+    try:
+        while True:                                      
+            csv_interval_gather(capture, allowed_IP)
+    except KeyboardInterrupt:
+        pass   
+    
+    packets = pd.read_csv(fileName, sep='","')
+    return(packets)
 
-    """
-    read the csv dataset
-    extract necessary details from the dataset
-    """  
 
-    def __init__(self, filename):
-        self.filename = 'packets.csv'
+def get_ip_layer_name(pkt): #allows the program to differentiate between ipv4 and ipv6, needed for correct parsing of packets
+    for layer in pkt.layers:
+        if layer._layer_name == 'ip':
+            return 4
+        elif layer._layer_name == 'ipv6':
+            return 6
 
-    def openFile(self):
-        packets = pd.read_csv(self.fileName, sep='","')
-        return(packets)
+def csv_interval_gather(cap, allowed_IP): # creates/rewrites 'Live.csv' file with 30 second intervals- writes header row - goes through packets, writing a row to the csv for each packet
+    start_time = time.time()
+    with open ('app/LiveAnn.csv', 'w', newline='') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',' , quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        filewriter.writerow(['Highest Layer', 'Transport Layer', 'Source IP', 'Dest IP', 'Source Port', 'Dest Port','Packet Length', 'Packets/Time'])
+
+        i = 0
+        start = timer()
+        for pkt in cap:
+            end = timer()
+            if (end - start < 30):
+                try:
+                    if pkt.highest_layer != 'ARP':
+                        print("Packets Collected:", i)
+                        if pkt.highest_layer != 'ARP':
+                            ip = None
+                            ip_layer = get_ip_layer_name(pkt)
+                            if ip_layer == 4:
+                                ip = pkt.ip
+                                #ipv = 0 # target test
+                                if pkt.transport_layer == None:
+                                    transport_layer = 'None'
+                                else:
+                                    transport_layer = pkt.transport_layer
+                            elif ip_layer == 6:
+                                ip = pkt.ipv6
+                                #ipv = 1 # target test
+                            try:
+                                if ip.src not in allowed_IP:
+                                        ipcat = 1
+                                else:
+                                        ipcat = 0
+                                filewriter.writerow([pkt.highest_layer, transport_layer, ipcat, ip.dst, pkt[pkt.transport_layer].srcport, pkt[pkt.transport_layer].dstport,pkt.length, i/(time.time() - start_time)])
+                                print ("Time: ", time.time() - start_time)
+                                i += 1
+                            except AttributeError:
+                                if ip.src not in allowed_IP:
+                                        ipcat = 1
+                                else:
+                                        ipcat = 0
+                                filewriter.writerow([pkt.highest_layer, transport_layer, ipcat, ip.dst, 0, 0, pkt.length, i/(time.time() - start_time)])
+                                print ("Time: ", time.time() - start_time)
+                                i += 1
+
+                        else:
+                            if pkt.arp.src_proto_ipv4 not in allowed_IP:
+                                    ipcat = 1
+                            else:
+                                    ipcat = 0
+                            arp = pkt.arp
+                            filewriter.writerow([pkt.highest_layer , transport_layer, ipcat, arp.dst_proto_ipv4, 0, 0, pkt.length, i/(time.time() - start_time)])
+                            print ("Time: ", time.time() - start_time)
+                            i += 1
+                except (UnboundLocalError, AttributeError) as e:
+                        pass
+            else:
+                return
+
+main()
+
+
+# # reads the input file and transforms it. 
+# import pandas as pd
+# import csv
+# import pyshark
+# import netifaces
+
+# class read_file:
+
+#     """
+#     read the csv dataset
+#     extract necessary details from the dataset
+#     """  
+
+#     def __init__(self, filename):
+#         int = netifaces.interfaces()
+
+#         self.cap = int_choice()
+#         self.filename = 'LiveAnn.csv'
+    
+
+#     def openFile(self):
+#         packets = pd.read_csv(self.fileName, sep='","')
+#         return(packets)
--- app/routes.py
+++ app/routes.py
@@ -7,14 +7,22 @@ from flask import render_template
 from app import app
 
 
-filepath = os.path.join(os.path.dirname(__file__), 'packets.csv')
+filepath = os.path.join(os.path.dirname(__file__), 'LiveAnn.csv')
+filepath_ = os.path.join(os.path.dirname(__file__), 'new.jpeg')
 packet_number = 0
 
 def readFile():
     # open_read = open(filepath, 'r')
-    df = pd.read_csv(filepath, delimiter='\\')
+    df = pd.read_csv(filepath, delimiter=",")
     # df = pd.DataFrame.from_csv(filepath)
     return df
+
+# page = readFile()
+# url_ = 'http://localhost:8000/master/packet'
+# # files = {'upload_file': open(filepath, 'rb')}
+# files = {'media': open(filepath_, 'rb')}
+# status = requests.post(url_, files=files)
+# print("status - ", status)
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -23,17 +31,15 @@ def index():
     while(True):
         page = readFile()
         url = "http://localhost:5000"
-        # sample packet details to be send to server
-        data = {'frame.number':'15',
-                'frame.time':'Mar 26, 2019 08:28:51.393156791 IST',
-                'eth.src':'48:e2:44:d2:8c:13',
-                'eth.dst':'a8:96:75:07:4b:51',
-                'ip.src':'192.168.43.196',
-                'ip.dst':'151.101.193.69',
-                'ip.proto':'6'
-                }
+        
+        # sending file to server
+        # url_ = 'http://127.0.0.1:8000/master/recievepacket'
+        # # files = {'media': open(filepath, 'rb')}
+        # status = requests.post(url_, files={filepath: f})
+        # print("status - ", status)
+
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        packet_number = data['frame.number']
+        # packet_number = data['frame.number']
         return render_template("index.html", title="DDOS Analyser", packets=page.to_html(), attack=detect_attack)
         r = requests.post(url, data=json.dumps(data), headers=headers)        
         
